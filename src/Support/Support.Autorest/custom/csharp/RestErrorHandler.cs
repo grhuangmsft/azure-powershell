f33fd59741cbaee8eba7b40d8910cace90cf5207
Grace Huang (from Dev Box) <grhuang@microsoft.com>
2024-03-14 18:04:00 -0400

    remove comments

--- src/Support/Support.Autorest/custom/csharp/RestErrorHandler.cs
+++ src/Support/Support.Autorest/custom/csharp/RestErrorHandler.cs
@@ -12,38 +12,13 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.Support.Cmdlets
     {
         public static void WriteError(this Cmdlet cmdlet, HttpResponseMessage responseMessage, Task<IErrorResponse> errorResponseTask, ref Task<bool> returnNow)
         {
-            var errorDetailsString = responseMessage.Content.ReadAsStringAsync().Result;
-
+            var errorString = responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
             cmdlet.WriteError(new ErrorRecord(new System.Exception(), null, ErrorCategory.InvalidOperation, null)
             {
-                ErrorDetails = new ErrorDetails(errorDetailsString) { RecommendedAction = string.Empty }
+                ErrorDetails = new ErrorDetails(errorString) { RecommendedAction = string.Empty }
             });
 
             returnNow = Task.FromResult(true);
-            //var errorResponse = errorResponseTask?.ConfigureAwait(false).GetAwaiter().GetResult();
-
-            //if (errorResponse?.Detail != null && errorResponse?.Detail.Count > 0)
-            //{
-            //    //var errorDetails = errorResponse.Detail;
-            //    //var errorDetailsString = "";
-
-            //    //foreach (var errorDetail in errorDetails)
-            //    //{
-            //    //    errorDetailsString += errorDetail.Message + " ";
-            //    //}
-            //    var errorDetailsString = responseMessage.Content.ReadAsStringAsync().Result;
-
-            //    cmdlet.WriteError(new ErrorRecord(new System.Exception(), null, ErrorCategory.InvalidOperation, null)
-            //    {
-            //        ErrorDetails = new ErrorDetails(errorDetailsString) { RecommendedAction = string.Empty }
-            //    });
-
-            //    returnNow = Task.FromResult(true);
-            //}
-            //else
-            //{
-            //    returnNow = Task.FromResult(false);
-            //}
         }
     }
 }
